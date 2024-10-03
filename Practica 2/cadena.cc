5a28bb2e79b14d9ce5d84b969b4e0db87a7a9ea3
Diegocordi <56880741+Diegocordi@users.noreply.github.com>
2024-10-03 19:49:27 +0100

    Update cadena.cc

---
+++ Practica 2/cadena.cc
@@ -0,0 +1,15 @@
+// Universidad de La Laguna
+// Escuela Superior de Ingenier ́ıa y Tecnolog ́ıa
+// Grado en Ingenier ́ıa Inform ́atica
+// Asignatura: Computabilidad y Algoritmia
+// Curso: 2o
+// Pr ́actica 2: Cadenas y lenguajes
+// Autor: Diego Alejandro Cordido Figueroa
+// Correo: alu0101229300@ull.edu.es
+// Fecha: 17/09/2024
+// Archivo cya-P02-strings.cc: programa cliente.
+// Contiene la funci ́on main del proyecto que usa las clases X e Y
+// para ... (indicar brevemente el objetivo)
+
+// Historial de revisiones
+// 17/09/2024 - Creaci ́on (primera versi ́on) del c ́odigo
