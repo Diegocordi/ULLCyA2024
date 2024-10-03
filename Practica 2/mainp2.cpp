dbbcf0b8f337c34d6323f7b6f10c1865549c6c44
Diegocordi <56880741+Diegocordi@users.noreply.github.com>
2024-10-03 19:48:37 +0100

    Update mainp2.cpp

--- Practica 2/mainp2.cpp
+++ Practica 2/mainp2.cpp
@@ -1,10 +1,18 @@
-/*
-Computabilidad y Algoritmia
-    Practica 2: Cadenas y Lenguajes
+// Universidad de La Laguna
+// Escuela Superior de Ingenier ́ıa y Tecnolog ́ıa
+// Grado en Ingenier ́ıa Inform ́atica
+// Asignatura: Computabilidad y Algoritmia
+// Curso: 2o
+// Pr ́actica 2: Cadenas y lenguajes
+// Autor: Diego Alejandro Cordido Figueroa
+// Correo: alu0101229300@ull.edu.es
+// Fecha: 17/09/2024
+// Archivo cya-P02-strings.cc: programa cliente.
+// Contiene la funci ́on main del proyecto que usa las clases X e Y
+// para ... (indicar brevemente el objetivo)
 
-Diego Alejandro Cordido Figueroa
-    alu0101229300
-*/
+// Historial de revisiones
+// 17/09/2024 - Creaci ́on (primera versi ́on) del c ́odigo
 
 #include <iostream>
 #include <regex>
