22c6db9736efd0396f8c886244078b8811e166d7
Diegocordi <56880741+Diegocordi@users.noreply.github.com>
2024-10-03 16:03:44 +0100

    Add files via upload

---
+++ Practica 2/mainp2.cpp
@@ -0,0 +1,17 @@
+/*
+Computabilidad y Algoritmia
+    Practica 2: Cadenas y Lenguajes
+
+Diego Alejandro Cordido Figueroa
+    alu0101229300
+*/
+
+#include <iostream>
+#include <regex>
+
+int main() {
+
+
+
+    return 0;
+}
---
+++ Practica 3/mainp3.cpp
@@ -0,0 +1,17 @@
+/*
+Computabilidad y Algoritmia
+    Practica 3: Expresiones regulares
+
+Diego Alejandro Cordido Figueroa
+    alu0101229300
+*/
+
+#include <iostream>
+#include <regex>
+
+int main(int argc, char* argv[]) {
+
+
+
+    return 0;
+}
