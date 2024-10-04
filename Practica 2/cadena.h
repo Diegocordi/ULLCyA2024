11253d4b5182a685ae2cd5a3ac229663cd32064a
Diegocordi <56880741+Diegocordi@users.noreply.github.com>
2024-10-04 19:47:46 +0100

    Update cadena.h

--- Practica 2/cadena.h
+++ Practica 2/cadena.h
@@ -1,9 +1,9 @@
 // Universidad de La Laguna
-// Escuela Superior de Ingenier ́ıa y Tecnolog ́ıa
-// Grado en Ingenier ́ıa Inform ́atica
+// Escuela Superior de Ingenierıa y Tecnologıa
+// Grado en Ingenierıa Informatica
 // Asignatura: Computabilidad y Algoritmia
-// Curso: 2o
-// Pr ́actica 2: Cadenas y lenguajes
+// Curso: 2
+// Practica 2: Cadenas y lenguajes
 // Autor: Diego Alejandro Cordido Figueroa
 // Correo: alu0101229300@ull.edu.es
 // Fecha: 17/09/2024
@@ -13,3 +13,38 @@
 
 // Historial de revisiones
 // 17/09/2024 - Creaci ́on (primera versi ́on) del c ́odigo
+
+// Iniciar guarda
+#ifndef CADENA_H
+#define CADENA_H
+
+#include <string>
+#include <set>
+
+class Cadena {
+  private:
+    std::string cadena_;            // Almacena la cadena de caracteres
+    std::set<char> alfabeto_;       // Almacena el alfabeto de la cadena
+
+  public:
+// Constructor que inicializa la cadena y su alfabeto
+  Cadena(const std::string& cadena, const std::set<char>& alfabeto);
+
+// Método que devuelve el alfabeto de la cadena
+  std::set<char> ObtenerAlfabeto() const;
+
+// Método que devuelve la longitud de la cadena
+  int Longitud() const;
+
+// Método que devuelve la inversa de la cadena
+  std::string Inversa() const;
+
+// Método que devuelve los prefijos de la cadena
+  std::set<std::string> Prefijos() const;
+
+// Método que devuelve los sufijos de la cadena
+  std::set<std::string> Sufijos() const;
+};
+
+// Finalizar guarda
+#endif CADENA_H
