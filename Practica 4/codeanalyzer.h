0361bca2fb665ddf075f6680da8fd146be5cc9cd
Diegocordi <56880741+Diegocordi@users.noreply.github.com>
2024-10-08 14:14:05 +0100

    Create codeanalyzer.h

---
+++ Practica 4/codeanalyzer.h
@@ -0,0 +1,51 @@
+#ifndef CODEANALYZER_H
+#define CODEANALYZER_H
+
+#include <string>
+#include <regex>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+class Variable {
+public:
+    std::string name;
+    std::string type;
+    int line;
+    bool initialized;
+
+    Variable(std::string n, std::string t, int l, bool init)
+        : name(n), type(t), line(l), initialized(init) {}
+};
+
+class Loop {
+public:
+    std::string type;
+    int line;
+
+    Loop(std::string t, int l) : type(t), line(l) {}
+};
+
+class Comment {
+public:
+    std::string content;
+    std::vector<int> lines;
+
+    Comment(std::string c, int line) : content(c) {
+        lines.push_back(line);
+    }
+};
+
+class CodeAnalyzer {
+private:
+    std::vector<Variable> variables;
+    std::vector<Loop> loops;
+    std::vector<Comment> comments;
+    bool mainExists = false;
+
+public:
+    void analyze(const std::string& inputFile);
+    void printResults(const std::string& outputFile);
+};
+
+#endif
